e831dbf8751884ce4192c36484777772cd0be73b
laura.baluchova <laura.menova@gmail.com>
2023-03-22 10:47:14 +0100

    calculation adjusted

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -3,11 +3,12 @@ import ChartBar from "./ChartBar";
 
 const Chart = ({ dataPoints }) => {
     const dataPointValues = dataPoints.map(dataPoint => dataPoint.value);
-    const totalMax = Math.max(...dataPointValues);
+    const totalSum = dataPointValues.reduce((acc, currentValue) => acc + currentValue);
+    console.log(dataPointValues, totalSum)
 
     return (
         <div className="chart">
-            {dataPoints.map((dataPoint) => (<ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMax} label={dataPoint.label} />))}
+            {dataPoints.map((dataPoint) => (<ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalSum} label={dataPoint.label} />))}
         </div>
     );
 };
